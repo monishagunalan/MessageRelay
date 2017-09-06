3aa99e6a228249b96870863677951a86d6378f71
Monisha Gunalan <monisha.gunalan@youi.tv>
2017-09-06 15:21:53 -0400

    Moved aws calls to separate file

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -1,14 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"net/http/httputil"
-
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/awserr"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/dynamodb"
 )
 
 var tableName = "SerialHostLookup"
@@ -23,134 +17,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	proxy.ServeHTTP(w, r)
 }
 
-func awsDynamo(w http.ResponseWriter, r *http.Request) {
-	deviceID := r.URL.Query()["deviceId"][0]
-
-	var host = getHost(deviceID)
-	fmt.Println("get host: ", host)
-
-	deleteHost(deviceID)
-
-	setHost(deviceID)
-}
-
 func main() {
-	http.HandleFunc("/awsDynamo", awsDynamo)
 	http.HandleFunc("/receiveMsg", handler)
 	panic(http.ListenAndServe(":8080", nil))
-}
-
-func getHost(serial string) string {
-	host := ""
-	svc := dynamodb.New(session.New(), aws.NewConfig().WithRegion(region))
-	input := &dynamodb.GetItemInput{
-		Key: map[string]*dynamodb.AttributeValue{
-			"key": {
-				S: aws.String(serial),
-			},
-		},
-		TableName: aws.String(tableName),
-	}
-	result, err := svc.GetItem(input)
-
-	if err != nil {
-		if aerr, ok := err.(awserr.Error); ok {
-			switch aerr.Code() {
-			case dynamodb.ErrCodeProvisionedThroughputExceededException:
-				fmt.Println(dynamodb.ErrCodeProvisionedThroughputExceededException, aerr.Error())
-			case dynamodb.ErrCodeResourceNotFoundException:
-				fmt.Println(dynamodb.ErrCodeResourceNotFoundException, aerr.Error())
-			case dynamodb.ErrCodeInternalServerError:
-				fmt.Println(dynamodb.ErrCodeInternalServerError, aerr.Error())
-			default:
-				fmt.Println(aerr.Error())
-			}
-		} else {
-			// Print the error, cast err to awserr.Error to get the Code and Message from an error.
-			fmt.Println(err.Error())
-		}
-	} else {
-		if result.Item != nil {
-			host = *result.Item["host"].S
-		}
-	}
-	return host
-}
-
-func deleteHost(serial string) {
-	svc := dynamodb.New(session.New(), aws.NewConfig().WithRegion(region))
-	input := &dynamodb.DeleteItemInput{
-		Key: map[string]*dynamodb.AttributeValue{
-			"key": {
-				S: aws.String(serial),
-			},
-		},
-		TableName: aws.String(tableName),
-	}
-	result, err := svc.DeleteItem(input)
-
-	if err != nil {
-		if aerr, ok := err.(awserr.Error); ok {
-			switch aerr.Code() {
-			case dynamodb.ErrCodeConditionalCheckFailedException:
-				fmt.Println(dynamodb.ErrCodeConditionalCheckFailedException, aerr.Error())
-			case dynamodb.ErrCodeProvisionedThroughputExceededException:
-				fmt.Println(dynamodb.ErrCodeProvisionedThroughputExceededException, aerr.Error())
-			case dynamodb.ErrCodeResourceNotFoundException:
-				fmt.Println(dynamodb.ErrCodeResourceNotFoundException, aerr.Error())
-			case dynamodb.ErrCodeItemCollectionSizeLimitExceededException:
-				fmt.Println(dynamodb.ErrCodeItemCollectionSizeLimitExceededException, aerr.Error())
-			case dynamodb.ErrCodeInternalServerError:
-				fmt.Println(dynamodb.ErrCodeInternalServerError, aerr.Error())
-			default:
-				fmt.Println(aerr.Error())
-			}
-		} else {
-			// Print the error, cast err to awserr.Error to get the Code and Message from an error.
-			fmt.Println(err.Error())
-		}
-	} else {
-		fmt.Println(result)
-	}
-}
-
-func setHost(serial string) {
-	host := "127.0.0.1:8085"
-	svc := dynamodb.New(session.New(), aws.NewConfig().WithRegion(region))
-	input := &dynamodb.PutItemInput{
-		Item: map[string]*dynamodb.AttributeValue{
-			"key": {
-				S: aws.String(serial),
-			},
-			"host": {
-				S: aws.String(host),
-			},
-		},
-		TableName: aws.String(tableName),
-	}
-	result, err := svc.PutItem(input)
-
-	if err != nil {
-		if aerr, ok := err.(awserr.Error); ok {
-			switch aerr.Code() {
-			case dynamodb.ErrCodeConditionalCheckFailedException:
-				fmt.Println(dynamodb.ErrCodeConditionalCheckFailedException, aerr.Error())
-			case dynamodb.ErrCodeProvisionedThroughputExceededException:
-				fmt.Println(dynamodb.ErrCodeProvisionedThroughputExceededException, aerr.Error())
-			case dynamodb.ErrCodeResourceNotFoundException:
-				fmt.Println(dynamodb.ErrCodeResourceNotFoundException, aerr.Error())
-			case dynamodb.ErrCodeItemCollectionSizeLimitExceededException:
-				fmt.Println(dynamodb.ErrCodeItemCollectionSizeLimitExceededException, aerr.Error())
-			case dynamodb.ErrCodeInternalServerError:
-				fmt.Println(dynamodb.ErrCodeInternalServerError, aerr.Error())
-			default:
-				fmt.Println(aerr.Error())
-			}
-		} else {
-			// Print the error, cast err to awserr.Error to get the Code and Message from an error.
-			fmt.Println(err.Error())
-		}
-	} else {
-		fmt.Println(result)
-	}
 }
