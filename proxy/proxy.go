b2d7900e6b342aaceb62b0645e28449543b9002f
Monisha Gunalan <monisha.gunalan@youi.tv>
2017-09-06 14:32:37 -0400

    get host from dynamo

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -12,16 +12,39 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	director := func(req *http.Request) {
+		req.URL.Scheme = "http"
+		req.URL.Host = "127.0.0.1:8081"
+	}
+	proxy := &httputil.ReverseProxy{Director: director}
+	proxy.ServeHTTP(w, r)
+}
 
-	// svc := dynamodb.New(session.New())
-	svc := dynamodb.New(session.New(), aws.NewConfig().WithRegion("us-east-1"))
+func awsDynamo(w http.ResponseWriter, r *http.Request) {
+	deviceID := r.URL.Query()["deviceId"][0]
+
+	var host = getHost(deviceID)
+	fmt.Println("get host: ", host)
+}
+
+func main() {
+	http.HandleFunc("/awsDynamo", awsDynamo)
+	http.HandleFunc("/receiveMsg", handler)
+	panic(http.ListenAndServe(":8080", nil))
+}
+
+func getHost(serial string) string {
+	tableName := "SerialHostLookup"
+	region := "us-east-1"
+	host := ""
+	svc := dynamodb.New(session.New(), aws.NewConfig().WithRegion(region))
 	input := &dynamodb.GetItemInput{
 		Key: map[string]*dynamodb.AttributeValue{
-			"sessionId": {
-				S: aws.String("bb043bb6-2118-47fb-8206-1539be03a57f"),
+			"key": {
+				S: aws.String(serial),
 			},
 		},
-		TableName: aws.String("filmstruckdev_dev_EngineSession"),
+		TableName: aws.String(tableName),
 	}
 	result, err := svc.GetItem(input)
 
@@ -38,25 +61,11 @@ func handler(w http.ResponseWriter, r *http.Request) {
 				fmt.Println(aerr.Error())
 			}
 		} else {
-			// Print the error, cast err to awserr.Error to get the Code and
-			// Message from an error.
+			// Print the error, cast err to awserr.Error to get the Code and Message from an error.
 			fmt.Println(err.Error())
 		}
-		return
+	} else {
+		host = *result.Item["host"].S
 	}
-
-	fmt.Println(result)
-
-	director := func(req *http.Request) {
-		req.URL.Scheme = "http"
-		req.URL.Host = "127.0.0.1:8081"
-	}
-	proxy := &httputil.ReverseProxy{Director: director}
-	proxy.ServeHTTP(w, r)
-}
-
-func main() {
-	http.HandleFunc("/receiveMsg", handler)
-	panic(http.ListenAndServe(":8080", nil))
-
+	return host
 }
