383015cf1064031edea43811620e2083f8bdfdd4
Monisha Gunalan <monisha.gunalan@youi.tv>
2017-09-06 15:15:33 -0400

    set and delete host on the dynamo table

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -28,6 +28,10 @@ func awsDynamo(w http.ResponseWriter, r *http.Request) {
 
 	var host = getHost(deviceID)
 	fmt.Println("get host: ", host)
+
+	deleteHost(deviceID)
+
+	setHost(deviceID)
 }
 
 func main() {
@@ -71,4 +75,82 @@ func getHost(serial string) string {
 		}
 	}
 	return host
+}
+
+func deleteHost(serial string) {
+	svc := dynamodb.New(session.New(), aws.NewConfig().WithRegion(region))
+	input := &dynamodb.DeleteItemInput{
+		Key: map[string]*dynamodb.AttributeValue{
+			"key": {
+				S: aws.String(serial),
+			},
+		},
+		TableName: aws.String(tableName),
+	}
+	result, err := svc.DeleteItem(input)
+
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			case dynamodb.ErrCodeConditionalCheckFailedException:
+				fmt.Println(dynamodb.ErrCodeConditionalCheckFailedException, aerr.Error())
+			case dynamodb.ErrCodeProvisionedThroughputExceededException:
+				fmt.Println(dynamodb.ErrCodeProvisionedThroughputExceededException, aerr.Error())
+			case dynamodb.ErrCodeResourceNotFoundException:
+				fmt.Println(dynamodb.ErrCodeResourceNotFoundException, aerr.Error())
+			case dynamodb.ErrCodeItemCollectionSizeLimitExceededException:
+				fmt.Println(dynamodb.ErrCodeItemCollectionSizeLimitExceededException, aerr.Error())
+			case dynamodb.ErrCodeInternalServerError:
+				fmt.Println(dynamodb.ErrCodeInternalServerError, aerr.Error())
+			default:
+				fmt.Println(aerr.Error())
+			}
+		} else {
+			// Print the error, cast err to awserr.Error to get the Code and Message from an error.
+			fmt.Println(err.Error())
+		}
+	} else {
+		fmt.Println(result)
+	}
+}
+
+func setHost(serial string) {
+	host := "127.0.0.1:8085"
+	svc := dynamodb.New(session.New(), aws.NewConfig().WithRegion(region))
+	input := &dynamodb.PutItemInput{
+		Item: map[string]*dynamodb.AttributeValue{
+			"key": {
+				S: aws.String(serial),
+			},
+			"host": {
+				S: aws.String(host),
+			},
+		},
+		TableName: aws.String(tableName),
+	}
+	result, err := svc.PutItem(input)
+
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			case dynamodb.ErrCodeConditionalCheckFailedException:
+				fmt.Println(dynamodb.ErrCodeConditionalCheckFailedException, aerr.Error())
+			case dynamodb.ErrCodeProvisionedThroughputExceededException:
+				fmt.Println(dynamodb.ErrCodeProvisionedThroughputExceededException, aerr.Error())
+			case dynamodb.ErrCodeResourceNotFoundException:
+				fmt.Println(dynamodb.ErrCodeResourceNotFoundException, aerr.Error())
+			case dynamodb.ErrCodeItemCollectionSizeLimitExceededException:
+				fmt.Println(dynamodb.ErrCodeItemCollectionSizeLimitExceededException, aerr.Error())
+			case dynamodb.ErrCodeInternalServerError:
+				fmt.Println(dynamodb.ErrCodeInternalServerError, aerr.Error())
+			default:
+				fmt.Println(aerr.Error())
+			}
+		} else {
+			// Print the error, cast err to awserr.Error to get the Code and Message from an error.
+			fmt.Println(err.Error())
+		}
+	} else {
+		fmt.Println(result)
+	}
 }
